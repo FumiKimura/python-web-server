9ca4f547f1776d41158f3d5000b95cc84f434f8a
Norifumi Kimura <norical496@gmail.com>
2021-11-28 14:25:03 +0900

    Return HTML in given path and 404 if does not exist

--- webserver.py
+++ webserver.py
@@ -1,8 +1,12 @@
 import socket
+import os
 from datetime import datetime
 
 
 class WebServer:
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
     def serve(self):
         print("starting server...")
@@ -23,18 +27,33 @@ class WebServer:
             with open("server_recv.txt", "wb") as f:
                 f.write(request)
 
-            response_body = "<html><body><h1>It works!</h1></body></html>"
+            request_line, remain = request.split(b"\r\n", maxsplit=1)
+            request_header, request_body = remain.split(
+                b"\r\n\r\n", maxsplit=1)
+
+            method, path, http_version = request_line.decode().split(" ")
+            relative_path = path.lstrip("/")
+            static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+
+            try:
+
+                with open(static_file_path, "rb") as f:
+                    response_body = f.read()
+
+            except OSError:
+                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                response_line = "HTTP/1.1 404 NOT FOUND \r\n"
 
             response_line = "HTTP/1.1 200 OK\r\n"
             response_header = ""
             response_header += f"Date: {datetime.utcnow().strftime('%a, %b %d %Y %H:%M:%S GMT')}\r\n"
             response_header += "Host: HenaServer/0.1\r\n"
-            response_header += f"Content-Length: {len(response_body.encode())}\r\n"
+            response_header += f"Content-Length: {len(response_body)}\r\n"
             response_header += "Connection: Close\r\n"
             response_header += "Content-Type: text/html\r\n"
 
             response = (response_line + response_header +
-                        "\r\n" + response_body).encode()
+                        "\r\n").encode() + response_body
             client_socket.send(response)
 
             client_socket.close()
