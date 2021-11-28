c91f972ae1bf6c4f6f6749f814ad742b3cce5a7e
Norifumi Kimura <norical496@gmail.com>
2021-11-28 19:06:20 +0900

    Cleaned up the code as written in Chapter13

--- webserver.py
+++ webserver.py
@@ -2,6 +2,7 @@ import socket
 import os
 import traceback
 from datetime import datetime
+from typing import Tuple
 
 
 class WebServer:
@@ -21,11 +22,7 @@ class WebServer:
         print("starting server...")
 
         try:
-            server_socket = socket.socket()
-            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-            server_socket.bind(("127.0.0.1", 8080))
-            server_socket.listen(10)
+            server_socket = self.create_server_socket()
 
             while True:
 
@@ -34,60 +31,82 @@ class WebServer:
                 print(
                     f"connection with client ended.. remote_address: {address}")
 
-                request = client_socket.recv(4096)
                 try:
-                    with open("server_recv.txt", "wb") as f:
-                        f.write(request)
-
-                    request_line, remain = request.split(b"\r\n", maxsplit=1)
-                    request_header, request_body = remain.split(
-                        b"\r\n\r\n", maxsplit=1)
-
-                    method, path, http_version = request_line.decode().split(" ")
-                    relative_path = path.lstrip("/")
-                    static_file_path = os.path.join(
-                        self.STATIC_ROOT, relative_path)
-
-                    try:
-
-                        with open(static_file_path, "rb") as f:
-                            response_body = f.read()
-
-                    except OSError:
-                        response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                        response_line = "HTTP/1.1 404 NOT FOUND \r\n"
-
-                    if "." in path:
-                        ext = path.rsplit(".", maxsplit=1)[-1]
-                    else:
-                        ext = ""
-
-                    content_type = self.MIME_TYPES.get(
-                        ext, "application/octet-stream")
-
-                    response_line = "HTTP/1.1 200 OK\r\n"
-                    response_header = ""
-                    response_header += f"Date: {datetime.utcnow().strftime('%a, %b %d %Y %H:%M:%S GMT')}\r\n"
-                    response_header += "Host: HenaServer/0.1\r\n"
-                    response_header += f"Content-Length: {len(response_body)}\r\n"
-                    response_header += "Connection: Close\r\n"
-                    response_header += f"Content-Type: {content_type}\r\n"
-
-                    response = (response_line + response_header +
-                                "\r\n").encode() + response_body
-                    client_socket.send(response)
-
-                    client_socket.close()
+                    self.handle_client(client_socket)
 
                 except Exception:
                     print("There was an error while returning response")
                     traceback.print_exec()
 
                 finally:
+                    print("Ending connection to client...")
                     client_socket.close()
 
         finally:
-            print("stopping server...")
+            print("Stopping server...")
+
+    def create_server_socket(self) -> socket:
+        server_socket = socket.socket()
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        server_socket.bind(("127.0.0.1", 8080))
+        server_socket.listen(10)
+
+        return server_socket
+
+    def handle_client(self, client_socket: socket) -> None:
+        request = client_socket.recv(4096)
+
+        with open("server_recv.txt", "wb") as f:
+            f.write(request)
+
+        method, path, http_version, request_header, request_body = self.parse_http_request(
+            request)
+
+        try:
+            response_body = self.get_static_file_content(path)
+            response_line = "HTTP/1.1 200 OK\r\n"
+
+        except OSError:
+            response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+            response_line = "HTTP/1.1 404 NOT FOUND \r\n"
+
+        response_header = self.build_response_header(path, response_body)
+        response = (response_line + response_header +
+                    "\r\n").encode() + response_body
+
+        client_socket.send(response)
+
+    def parse_http_request(self, request: bytes) -> Tuple[str, str, str, bytes, bytes]:
+        request_line, remain = request.split(b"\r\n", maxsplit=1)
+        request_header, request_body = remain.split(
+            b"\r\n\r\n", maxsplit=1)
+
+        method, path, http_version = request_line.decode().split(" ")
+
+        return method, path, http_version, request_header, request_body
+
+    def get_static_file_content(self, path: str) -> bytes:
+        relative_path = path.lstrip("/")
+        static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+        with open(static_file_path, "rb") as f:
+            return f.read()
+
+    def build_response_header(self, path: str, response_body: bytes) -> str:
+        if "." in path:
+            ext = path.rsplit(".", maxsplit=1)[-1]
+        else:
+            ext = ""
+
+        content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+        response_header = ""
+        response_header += f"Date: {datetime.utcnow().strftime('%a, %b %d %Y %H:%M:%S GMT')}\r\n"
+        response_header += "Host: HenaServer/0.1\r\n"
+        response_header += f"Content-Length: {len(response_body)}\r\n"
+        response_header += "Connection: Close\r\n"
+        response_header += f"Content-Type: {content_type}\r\n"
+
+        return response_header
 
 
 if __name__ == '__main__':
